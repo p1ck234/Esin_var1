43817f9c6d311372903f66410196e53bc4021455
Danil <93711640+p1ck234@users.noreply.github.com>
2022-12-09 17:42:57 +0400

    dd

---
+++ var1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max;
+
+            List<int> list = new List<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(-1);
+            list.Add(4);
+            void MAX(List<int> _list)
+            {
+                max = list.Max();
+            }
+            MAX(list);
+            Console.Write(max);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
